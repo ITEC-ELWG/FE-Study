cdbaf4a47f6de61c7577a0e812989e5fa3b65a90
lidasong <1584809032@qq.com>
2014-03-03 09:36:44 +0800

    blog

--- blog/login_js.js
+++ blog/login_js.js
@@ -4,7 +4,7 @@ var pass_word=document.getElementById("password").value;
 var user_name=document.getElementById("username").value;
 if(pass_word=="lidasong2014"&&user_name=="lidasong")
 {
-    window.open("main_page.html","_self");
+    window.open("index.html","_self");
 } 
     else 
         alert("username:ldiasong\n\n password:lidasong2014");
