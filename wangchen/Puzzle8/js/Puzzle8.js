f8373dfef84f175edd9dbb852808b9fe0f80af8b
MrHowl <mrhowl@foxmail.com>
2014-03-13 21:54:09 +0800

    Puzzle 1.2
    已实现随时判定拼图是否正确。

--- wangchen/Puzzle8/js/Puzzle8.js
+++ wangchen/Puzzle8/js/Puzzle8.js
@@ -9,10 +9,13 @@ $(document).ready(function() {
 	});
 	$(document).ready(function() {
 		for (var i = 0; i < 8; i++) {
-			$($("span")[i]).text(grid[i]);
+			$($(".blockIn")[i]).text(grid[i]);
 		}
 	});
 	grid[8] = '0';
+	$(".buttonReset").click(function() {
+		$(".result").load('js/demo.html');
+	});
 });
 
 function getEventTarget(e) {
@@ -21,57 +24,56 @@ function getEventTarget(e) {
 	targ = e.target || e.srcElement;
 	var tId = targ.id;
 	$("#" + tId).unbind();
-	$("#" + tId).click(function() {
-		for (var i = -3; i < 4; i = i + 2) {
-			if (tId == check[empty + i]) {
-				var cache = check[empty + i];
-				check[empty + i] = check[empty];
-				check[empty] = cache;
-				var cache1 = grid[empty + i];
-				grid[empty + i] = grid[empty];
-				grid[empty] = cache1;
-				empty = empty + i;
-				switch (i) {
-					case -1:
-						$("#" + tId).animate({
-							left: '+=130px'
-						}, 300);
-						break;
-					case 1:
-						$("#" + tId).animate({
-							left: '-=130px'
-						}, 300);
-						break;
-					case -3:
-						$("#" + tId).animate({
-							top: '+=130px'
-						}, 300);
-						break;
-					case 3:
-						$("#" + tId).animate({
-							top: '-=130px'
-						}, 300);
-						break;
-				}
-				return;
+	for (var i = -3; i < 4; i = i + 2) {
+		if (tId == check[empty + i]) {
+			var cache = check[empty + i];
+			check[empty + i] = check[empty];
+			check[empty] = cache;
+			var cache1 = grid[empty + i];
+			grid[empty + i] = grid[empty];
+			grid[empty] = cache1;
+			empty = empty + i;
+			switch (i) {
+				case -1:
+					$("#" + tId).animate({
+						left: '+=130px'
+					}, 300);
+					break;
+				case 1:
+					$("#" + tId).animate({
+						left: '-=130px'
+					}, 300);
+					break;
+				case -3:
+					$("#" + tId).animate({
+						top: '+=130px'
+					}, 300);
+					break;
+				case 3:
+					$("#" + tId).animate({
+						top: '-=130px'
+					}, 300);
+					break;
 			}
-			continue;
+			if (grid.toString() == standard.toString()) {
+				$(".result").html("U did it !");
+			}
+			return;
 		}
-	});
-	if (grid[0] == standard[0]) {
-		$(".result").html("完成");
+		continue;
 	}
 }
 
-function compare() {
-	if (grid.toString() != standard.toString()) {
-		$(".result").html("未完成");
-		return;
-	}
-	else if (grid.toString() == standard.toString()) {
-		$(".result").html("完成");
-	}
-}
+
+
+
+
+
+
+
+
+
+
 
 
 
