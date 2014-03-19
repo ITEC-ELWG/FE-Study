6b5c1b171c03b1d6ea66348f6af371a8ba957fe6
lidasong <1584809032@qq.com>
2014-03-19 12:39:19 +0800

    Puzzle8

--- lidasong/Puzzle8/Puzzle8.js
+++ lidasong/Puzzle8/Puzzle8.js
@@ -42,7 +42,7 @@ function clickButton(id){
     buttonChoose = $('#'+id);
     blankGet = $("#blank");
     move();
-    setTimeout("suceessCheck()",500);
+    setTimeout("suceessCheck()",1000);
 
 }
 function move() {                   //经过比对,移动相应按钮
@@ -85,30 +85,30 @@ function move() {                   //经过比对,移动相应按钮
     }
 }
 function suceessCheck() {               //成功检测
-    if ($(buttonType[0]).offset().top > $(buttonType[3]).offset().top
-     || $(buttonType[3]).offset().top > $(buttonType[6]).offset().top
-     ||$(buttonType[7]).offset().left>blankGet.offset().left)
+    if ($('#'+buttonType[0]).offset().top > $('#'+buttonType[3]).offset().top
+     || $('#'+buttonType[3]).offset().top > $('#'+buttonType[6]).offset().top
+     ||$('#'+buttonType[7]).offset().left>blankGet.offset().left)
         return;
-    if ($(buttonType[0]).offset().top == $(buttonType[1]).offset().top 
-      && $(buttonType[1]).offset().top == $(buttonType[2]).offset().top)
+    if ($('#'+buttonType[0]).offset().top == $('#'+buttonType[1]).offset().top 
+      && $('#'+buttonType[1]).offset().top == $('#'+buttonType[2]).offset().top)
 
     {
-        if ($(buttonType[0]).offset().left + 100 == $(buttonType[1]).offset().left 
-          && $(buttonType[1]).offset().left + 100 == $(buttonType[2]).offset().left) 
+        if ($('#'+buttonType[0]).offset().left + 100 == $('#'+buttonType[1]).offset().left 
+          && $('#'+buttonType[1]).offset().left + 100 == $('#'+buttonType[2]).offset().left) 
           {} 
         else return;
     } 
     else return;
-    if ($(buttonType[3]).offset().top == $(buttonType[4]).offset().top 
-      && $(buttonType[4]).offset().top == $(buttonType[5]).offset().top) {
-        if ($(buttonType[3]).offset().left + 100 == $(buttonType[4]).offset().left
-         && $(buttonType[4]).offset().left + 100 == $(buttonType[5]).offset().left) 
+    if ($('#'+buttonType[3]).offset().top == $('#'+buttonType[4]).offset().top 
+      && $('#'+buttonType[4]).offset().top == $('#'+buttonType[5]).offset().top) {
+        if ($('#'+buttonType[3]).offset().left + 100 == $('#'+buttonType[4]).offset().left
+         && $('#'+buttonType[4]).offset().left + 100 == $('#'+buttonType[5]).offset().left) 
           {}
            else return;
     }
      else return;
-    if ($(buttonType[6]).offset().top == $(buttonType[7]).offset().top 
-      && $(buttonType[6]).offset().left + 100 == $(buttonType[7]).offset().left)
+    if ($('#'+buttonType[6]).offset().top == $('#'+buttonType[7]).offset().top 
+      && $('#'+buttonType[6]).offset().left + 100 == $('#'+buttonType[7]).offset().left)
         alert("You succeed,Congratulations!");
     else return;
 }
