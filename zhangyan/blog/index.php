a611807e7cbd670b1ac3108cc282040820b3f748
zhangyan123 <1450802680@qq.com>
2014-03-29 13:50:55 +0800

    blog modification

--- zhangyan/blog/index.php
+++ zhangyan/blog/index.php
@@ -1,21 +1,14 @@
-<html>
-<head>
-	<title>login</title>
-</head>
-<body>
-	<h2>
-	    <?php
-			$email=$_POST['email'];
-			$password=$_POST['password'];
-			if($email=="1450802680@qq.com"&&$password=="A199103252020")
-			{
-				echo '<a href="http://zhangyan123.github.com"> click here to enter my home! </a>';
-			}
-			else
-			{ 
-				echo "sorry!wrong email or password,please try again!";
-			}
-        ?>
-	</h2>
-</body>
-</html>
+<?php
+		
+	$email=$_POST['email'];
+	$password=$_POST['password'];
+	if($email=="1450802680@qq.com"&&$password=="A199103252020")
+	{
+		header("location:http://zhangyan123.github.io/");
+	}
+	else
+	{ 
+		echo "sorry!wrong email or password,please try again!";
+	}
+?>
+	
