f38e76ffeac39e5b9c0fdbc9b393142b9402b483
liugui <1169430612@qq.com>
2014-10-16 10:50:43 +0800

    success js

--- liugui/jsstudy/js/js.js
+++ liugui/jsstudy/js/js.js
@@ -1,10 +1,9 @@
-function writesome ()
-{
-var pi = 3.14;
-var x = "my name is liugui";
-var y = "so great";
+function writesome() {
+    var pi = 3.14;
+    var x = "my name is liugui";
+    var y = "so great";
 
-document.write (pi + <br/>);
-document.write (x + <br/>);
-document.write (y + <br/>);
+    document.write(pi + "<br/>");
+    document.write(x + "<br/>");
+    document.write(y + "<br/>");
 }
