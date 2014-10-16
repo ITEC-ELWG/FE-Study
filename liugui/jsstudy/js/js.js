ad61cc3e5851d9084789c3e74655cf41d850c756
liugui <1169430612@qq.com>
2014-10-16 14:27:05 +0800

    about object

--- liugui/jsstudy/js/js.js
+++ liugui/jsstudy/js/js.js
@@ -5,5 +5,13 @@ function writesome() {
 
     document.write(pi + "<br/>");
     document.write(x + "<br/>");
-    document.write(y + "<br/>");
+    document.write(y + "<br/>");//写在函数内部的语句函数调用时才会执行，写在函数外面的语句浏览器载入时就会自动运行
+
+    var person = new Object()
+    person.name = "liugui"; //这时候属性之间可以用分号分隔
+    person.age = 56;
+    person.eyecolor = "black";
+
+    var tep = person.age;
+    document.write(tep + "<br/>");
 }
