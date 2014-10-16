d5790c57ba4123ba8e666b2fc31bc41aae96c6a9
liuweifly <liuweifly@yahoo.com>
2014-10-16 20:49:38 +0800

    add Cal files

---
+++ liuwei/Calculator/cal.js
@@ -0,0 +1,9 @@
+function command(num){
+documen.write(num); 
+// var str=String(document.calculator.numScreen.value); //获得当前显示数据 
+// str=(str!="0") ? ((operate==0) ? str : "") : ""; //如果当前值不是"0"，且状态为0，则返回当前值，否则返回空值; 
+// str=str + String(num); //给当前值追加字符 
+// document.calculator.numScreen.value=str; //刷新显示 
+// operate=0; //重置输入状态 
+// quit=0; //重置防止重复按键的标志 
+} 
