bcaf869a4769a0c26f8425befac81350ce4c7c3d
liugui <1169430612@qq.com>
2014-10-16 10:39:41 +0800

    test2

---
+++ liugui/jsstudy/js/js.js
@@ -0,0 +1,10 @@
+function writesome ()
+{
+var pi = 3.14;
+var x = "my name is liugui";
+var y = "so great";
+
+document.write (pi + <br/>);
+document.write (x + <br/>);
+document.write (y + <br/>);
+}
