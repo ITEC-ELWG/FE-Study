a14830c87d4b1eabc08cc452c448d4a0c5776708
liugui <1169430612@qq.com>
2014-10-18 19:20:23 +0800

    界面风格修改，用input代替button

--- liugui/calculator/js/js.js
+++ liugui/calculator/js/js.js
@@ -1,5 +1,51 @@
-function numinput()
-{
-	var num = document.getElementById("led");
-    num.innerHTML = 9;
+var num = document.getElementById("led");
+
+function clearall() {
+    num.innerHTML = "0"
 }
+
+function numinput9() {
+    num.innerHTML += "9";
+}
+
+function numinput8() {
+    num.innerHTML += "8";
+}
+
+function numinput7() {
+    num.innerHTML += "7";
+}
+
+function numinput6() {
+    num.innerHTML += "6";
+}
+
+function numinput5() {
+    num.innerHTML += "5";
+}
+
+function numinput4() {
+    num.innerHTML += "4";
+}
+
+function numinput3() {
+    num.innerHTML += "3";
+}
+
+function numinput2() {
+    num.innerHTML += "2";
+}
+
+function numinput1() {
+    num.innerHTML += "1";
+}
+
+function numinput0() {
+    num.innerHTML += "0";
+}
+
+function numinputp() {
+    num.innerHTML += ".";
+}
+
+
