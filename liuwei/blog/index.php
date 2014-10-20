dd5bda270d2e0d1dede4c03343f1d98d50a04288
liuweifly <liuweifly@yahoo.com>
2014-10-20 21:48:04 +0800

    add index.php

--- liuwei/blog/index.php
+++ liuwei/blog/index.php
@@ -1,15 +1,15 @@
 <!doctype html>
 <html>
-
+<?php 
+    if(! isset($_COOKIE['username']) || ! isset($_COOKIE['password']))
+        echo '<meta http-equiv="refresh" content="1;url=login.php" />'
+?>
 <head>
     <meta charset="utf-8">
-    <title>LogIn</title>
+    <title>Blog</title>
     <link type="text/css" rel="stylesheet" href="style/reset.css">
     <link type="text/css" rel="stylesheet" href="style/main.css">
-    <!--[if IE 6]>
-<script type="text/javascript" src="js/DD_belatedPNG_0.0.8a-min.js"></script>
-<script type="text/javascript" src="js/ie6Fixpng.js"></script>
-<![endif]-->
+
 </head>
 
 <body>
@@ -21,46 +21,137 @@
                     <img src="img/logo1.png" alt="logo">
                 </a>
             </div>
-            <h3 class="welcome_title">欢迎登录</h3>
+            <h3 class="welcome_title">刘 苇</h3>
+            <h3 class="welcome_signature clearfix">Stay Hungry, Stay Foolish.</h3>
             <!-- <div class="comWidth">
 
 		</div> -->
         </div>
     </div>
 
-    <div class="regBox">
-        <div class="login_cont">
-            <ul class="login">
-                <li>
-                    <span class="reg_item"><i>*</i>账户名：</span>
-                    <div class="input_item">
-                        <input type="text" class="login_input user_icon">
-                    </div>
+    <div class="navBox">
+        <div class="comWidth clearfix">
+            <ul class="nav fl">
+                <li><a href="#" class="active">Index</a>
                 </li>
-                <li>
-                    <span class="reg_item"><i>*</i>密码：</span>
-                    <div class="input_item">
-                        <input type="password" class="login_input user_icon">
-                    </div>
+                <li><a href="#">Friends</a>
                 </li>
-                <!-- <li><span class="reg_item"><i>*</i>账户名：</span><div class="input_item"><input type="text" class="login_input user_icon"></div></li>
-			<li><span class="reg_item"><i>*</i>账户名：</span><div class="input_item"><input type="text" class="login_input user_icon"></div></li> -->
-                <li class="autoLogin">
-                    <span class="reg_item">&nbsp;</span>
-                    <input type="checkbox" id="t1" class="checked">
-                    <label for="t1">我同意什么什么条款</label>
+                <li><a href="#">Experience</a>
                 </li>
-                <li>
-                    <span class="reg_item">&nbsp;</span>
-                    <input type="button" value="" class="login_btn" onclick="window.open('blog.php','_self')">
+                <li><a href="#">Journey</a>
+                </li>
+                <li><a href="#">Basketball</a>
+                </li>
+                <li><a href="#">Books</a>
+                </li>
+                <li><a href="#">Photograph</a>
                 </li>
             </ul>
-
         </div>
     </div>
 
-    <div class="hr_25"></div>
-    <!-- <div class="footer">
+    <!-- <div class="hr_25"></div> -->
+
+    <div class="comWidth clearfix">
+        <div class="contentBox">
+            <div class="cb_tit">
+                <div class="cb_TitLeft fl">
+                    <a href="#" class="">
+                        <span class="cb_titFont">Experience</span>
+                    </a>
+                </div>
+                <div class="cb_TitCen fl"><a href="#" class="conTit">【武汉高校2014级新生好声音】征集令来啦！</a>
+                </div>
+                <div class="cb_TitRight fr">
+                    <span class="conDate">2014-7-25</span>
+                </div>
+            </div>
+            <div class="cb_content">
+                <p>亲爱的准大学生们，终于，你们迎来了这个长长的暑假…这美好的时光当然也要去做一些美好的事情啦</p>
+                </br>
+                <p>~~蹬蹬蹬蹬~~武汉高校2014级新生好声音活动开始啦！爱好唱歌？喜欢听歌？那就来吧！~~</p>
+                </br>
+                <p>参与条件：只要你是武汉任一高校的2014级新生就都可以报名参赛啦。（我们前期不会一一核实身份，但在颁奖前要核实获奖选手的新生身份哦，如果对不上号，只能将奖品顺移给下一位啦） 参与方式：关注微信公众号“武汉高校生活通”，搜索名字或微信号“gx_tong”或扫描二维码都可以的~~之后按提示即可参赛啦
+                </p>
+            </div>
+
+        </div>
+        <div class="contentBox">
+            <div class="cb_tit">
+                <div class="cb_TitLeft fl">
+                    <a href="#" class="">
+                        <span class="cb_titFont">Experience</span>
+                    </a>
+                </div>
+                <div class="cb_TitCen fl"><a href="#" class="conTit">【武汉高校2014级新生好声音】征集令来啦！</a>
+                </div>
+                <div class="cb_TitRight fr">
+                    <span class="conDate">2014-7-25</span>
+                </div>
+            </div>
+            <div class="cb_content">
+                <p>亲爱的准大学生们，终于，你们迎来了这个长长的暑假…这美好的时光当然也要去做一些美好的事情啦</p>
+                </br>
+                <p>~~蹬蹬蹬蹬~~武汉高校2014级新生好声音活动开始啦！爱好唱歌？喜欢听歌？那就来吧！~~</p>
+                </br>
+                <p>参与条件：只要你是武汉任一高校的2014级新生就都可以报名参赛啦。（我们前期不会一一核实身份，但在颁奖前要核实获奖选手的新生身份哦，如果对不上号，只能将奖品顺移给下一位啦） 参与方式：关注微信公众号“武汉高校生活通”，搜索名字或微信号“gx_tong”或扫描二维码都可以的~~之后按提示即可参赛啦
+                </p>
+            </div>
+
+        </div>
+        <div class="contentBox">
+            <div class="cb_tit">
+                <div class="cb_TitLeft fl">
+                    <a href="#" class="">
+                        <span class="cb_titFont">Experience</span>
+                    </a>
+                </div>
+                <div class="cb_TitCen fl"><a href="#" class="conTit">【武汉高校2014级新生好声音】征集令来啦！</a>
+                </div>
+                <div class="cb_TitRight fr">
+                    <span class="conDate">2014-7-25</span>
+                </div>
+            </div>
+            <div class="cb_content">
+                <p>亲爱的准大学生们，终于，你们迎来了这个长长的暑假…这美好的时光当然也要去做一些美好的事情啦</p>
+                </br>
+                <p>~~蹬蹬蹬蹬~~武汉高校2014级新生好声音活动开始啦！爱好唱歌？喜欢听歌？那就来吧！~~</p>
+                </br>
+                <p>参与条件：只要你是武汉任一高校的2014级新生就都可以报名参赛啦。（我们前期不会一一核实身份，但在颁奖前要核实获奖选手的新生身份哦，如果对不上号，只能将奖品顺移给下一位啦） 参与方式：关注微信公众号“武汉高校生活通”，搜索名字或微信号“gx_tong”或扫描二维码都可以的~~之后按提示即可参赛啦
+                </p>
+            </div>
+
+        </div>
+        <div class="contentBox">
+            <div class="cb_tit">
+                <div class="cb_TitLeft fl">
+                    <a href="#" class="">
+                        <span class="cb_titFont">Experience</span>
+                    </a>
+                </div>
+                <div class="cb_TitCen fl"><a href="#" class="conTit">【武汉高校2014级新生好声音】征集令来啦！</a>
+                </div>
+                <div class="cb_TitRight fr">
+                    <span class="conDate">2014-7-25</span>
+                </div>
+            </div>
+            <div class="cb_content">
+                <p>亲爱的准大学生们，终于，你们迎来了这个长长的暑假…这美好的时光当然也要去做一些美好的事情啦</p>
+                </br>
+                <p>~~蹬蹬蹬蹬~~武汉高校2014级新生好声音活动开始啦！爱好唱歌？喜欢听歌？那就来吧！~~</p>
+                </br>
+                <p>参与条件：只要你是武汉任一高校的2014级新生就都可以报名参赛啦。（我们前期不会一一核实身份，但在颁奖前要核实获奖选手的新生身份哦，如果对不上号，只能将奖品顺移给下一位啦） 参与方式：关注微信公众号“武汉高校生活通”，搜索名字或微信号“gx_tong”或扫描二维码都可以的~~之后按提示即可参赛啦
+                </p>
+            </div>
+
+        </div>
+        <div class="checkMoreBox"><a href="#" class="cmb_font">More&gt;&gt;</a>
+        </div>
+    </div>
+
+    <!-- <div class="hr_25"></div> -->
+
+    <div class="footer">
         <p><a href="#">博客简介</a><i>|</i><a href="#">博客公告</a><i>|</i>  <a href="#">招纳贤士</a><i>|</i><a href="#">联系我们</a><i>|</i>客服热线：400-675-1234</p>
         <span style="font-family:arial;">Copyright &copy;</span>
         <span>2006 - 2014 liuwei版权所有&nbsp;&nbsp;&nbsp;京ICP备09037834号&nbsp;&nbsp;&nbsp;京ICP证B1034-8373号&nbsp;&nbsp;&nbsp;某市公安局XX分局备案编号：123456789123</span>
@@ -78,7 +169,7 @@
                 <img src="img/webLogo.jpg" alt="logo">
             </a>
         </p>
-    </div> -->
+    </div>
 </body>
 
 </html>
