c87df56a64ba23a6ab1f466501cba844aa0e0bba
liugui <1169430612@qq.com>
2014-10-22 09:09:07 +0800

    修改注释

--- liugui/calculator/js/js.js
+++ liugui/calculator/js/js.js
@@ -1,18 +1,26 @@
-var intinput;//存储原始输入
-var changeinput = 0;//存储转换后的输入值
-var temp;//存储第一个输入的值
-var op;//存储操作符
-var outtag = 0;//标志是否已经输出，以便在等于之后能继续计算
-var result = 0;//储存计算的结果
+//存储原始输入
+var intinput;
+//存储转换后的输入值
+var changeinput = 0;
+//存储第一个输入的值
+var temp;
+//存储操作符
+var op;
+//标志是否已经输出，以便在等于之后能继续计算
+var outtag = 0;
+//储存计算的结果
+var result = 0;
 var pertag = 1;
-
-var display = document.getElementById("led");//显示元素用一个变量代替
-
-function numinput (num){//读取数字的输入
+//显示元素用一个变量代替
+var display = document.getElementById("led");
+//读取数字的输入
+function numinput (num){
 	if (changeinput <= 999999999){
-    intinput = num.value;//用这种方式读入button按钮输入的值！！！！value不仅可以用在button上面的显示，其值也是可以调用的
+    //用这种方式读入button按钮输入的值！！！！value不仅可以用在button上面的显示，其值也是可以调用的
+    intinput = num.value;
     display.innerHTML += intinput;
-    changeinput = parseFloat(display.innerHTML); //将文本转换成浮点数
+    //将文本转换成浮点数
+    changeinput = parseFloat(display.innerHTML); 
     }
     else {
     	display.innerHTML = "超范围，请按AC"
@@ -63,7 +71,8 @@ function percent100(per){
 	if (pertag==1){
 	var beforper = parseFloat(display.innerHTML);
 	var afterper = beforper * 100;
-	display.innerHTML = afterper + "%";//同时输出变量和特殊文本字符的方法
+  //同时输出变量和特殊文本字符的方法
+	display.innerHTML = afterper + "%";
 	pertag = 0;
     }
     else {
