de174b66b138d040d94cb9bc54a5b6b1f9de92f1
liugui <1169430612@qq.com>
2014-10-22 10:22:57 +0800

    修复一开始输入多个零的BUG问题

--- liugui/calculator/js/js.js
+++ liugui/calculator/js/js.js
@@ -13,70 +13,85 @@ var result = 0;
 var pertag = 1;
 //显示元素用一个变量代替
 var display = document.getElementById("led");
+display.innerHTML = "0";
 //读取数字的输入
-function numInput (num){
-	if (changeinput <= 999999999){
-    //用这种方式读入button按钮输入的值！！！！value不仅可以用在button上面的显示，其值也是可以调用的
-    intInput = num.value;
-    display.innerHTML += intInput;
-    //将文本转换成浮点数
-    changeinput = parseFloat(display.innerHTML); 
-    }
-    else {
-    	display.innerHTML = "超范围，请按AC"
-    }
-}
+function numInput(num) {
+    if (changeinput <= 999999999) {
+        //用这种方式读入button按钮输入的值！！！！value不仅可以用在button上面的显示，其值也是可以调用的
+        intInput = num.value;
+        //修复一开始能输入多个0的BUG问题
+        if (display.innerHTML == "0") {
+            if (intInput == "0")
+                display.innerHTML = "0";
 
-function operate_sign(acc){
-    op = acc.value;
-    if (op == "AC"){
-       display.innerHTML = "";
-       outtag="0";
-       result = "0";
-       changeinput = 0;
-       pertag = 1;
-   }
-   else if ((op == "+")||(op == "-")||(op == "×")||(op == "÷")){
-   	    temp = (outtag == 1) ? result : changeinput;
-   	    intInput = "";
-   	    changeinput = "0";
-   	    display.innerHTML = "";
-   	    pertag = 1;
+            else {
+                display.innerHTML = "";
+                display.innerHTML += intInput;
+            }
         }
-}
+        else{ 
+          display.innerHTML += intInput;
+        }
+        
+        //将文本转换成浮点数
+        changeinput = parseFloat(display.innerHTML);
 
-function getResult(equal){
-	switch(op){
-		case "+":result = temp + changeinput;
-		break;
-		case "-":result = temp - changeinput;
-		break;
-		case "×":result = temp * changeinput;
-		break;
-		case "÷":result = temp / changeinput;
-		break;
-	}
-	if (result <= 999999999)
-	{
-	display.innerHTML = result;
-	outtag = 1;
-    }
-    else {
-    	display.innerHTML = "超范围，请按AC"
+
+
+    } else {
+        display.innerHTML = "超范围，请按AC"
     }
 }
 
-
-function percent100(per){
-	if (pertag==1){
-	var beforper = parseFloat(display.innerHTML);
-	var afterper = beforper * 100;
-  //同时输出变量和特殊文本字符的方法
-	display.innerHTML = afterper + "%";
-	pertag = 0;
-    }
-    else {
-    	display.innerHTML = display.innerHTML;
+function operate_sign(acc) {
+    op = acc.value;
+    if (op == "AC") {
+        display.innerHTML = "0";
+        outtag = "0";
+        result = "0";
+        changeinput = 0;
+        pertag = 1;
+    } else if ((op == "+") || (op == "-") || (op == "×") || (op == "÷")) {
+        temp = (outtag == 1) ? result : changeinput;
+        intInput = "";
+        changeinput = "0";
+        display.innerHTML = "";
+        pertag = 1;
     }
 }
 
+function getResult(equal) {
+    switch (op) {
+        case "+":
+            result = temp + changeinput;
+            break;
+        case "-":
+            result = temp - changeinput;
+            break;
+        case "×":
+            result = temp * changeinput;
+            break;
+        case "÷":
+            result = temp / changeinput;
+            break;
+    }
+    if (result <= 999999999) {
+        display.innerHTML = result;
+        outtag = 1;
+    } else {
+        display.innerHTML = "超范围，请按AC"
+    }
+}
+
+
+function percent100(per) {
+    if (pertag == 1) {
+        var beforper = parseFloat(display.innerHTML);
+        var afterper = beforper * 100;
+        //同时输出变量和特殊文本字符的方法
+        display.innerHTML = afterper + "%";
+        pertag = 0;
+    } else {
+        display.innerHTML = display.innerHTML;
+    }
+}
