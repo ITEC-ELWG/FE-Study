2608fc45b55361b530f07a9860cb2578a2d829c5
liuweifly <liuweifly@yahoo.com>
2014-10-24 16:40:34 +0800

    receiver js number and return corresponding array

--- liuwei/Puzzle8/puzzle.php
+++ liuwei/Puzzle8/puzzle.php
@@ -1,10 +1,12 @@
 <?php
 
-	for($i=1;$i<=9;$i++)
+	$num=$_POST['num'];
+	
+	for($i=1;$i<=$num*$num;$i++)
 	{
 		$my_array[$i] = $i; 
 	}
-	
+
 	shuffle($my_array);
 	// print_r($my_array);
     echo json_encode($my_array); 
