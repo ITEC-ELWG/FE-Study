158c0403b6493449d8c344e927cb53274e132d0a
liugui <1169430612@qq.com>
2014-11-04 20:53:09 +0800

    优化添加元素的方法，修复移动后无法重置的bug问题

--- liugui/Puzzle8/js/js.js
+++ liugui/Puzzle8/js/js.js
@@ -22,7 +22,7 @@ function valueGet(e) {
 
 function set(nowStatus, setStatus) {
     //根据给定的initStatus值设定nowStatues值并且排列界面！
-    for (var i = 0; i < 9; i++) {
+    for (var i = 0; i < n * n; i++) {
         if (nowStatus[i] != 8) {
             var setNum = nowStatus[i] + 1;
             $("[value=" + setStatus[i] + "]").text(setNum)
@@ -31,7 +31,6 @@ function set(nowStatus, setStatus) {
                 .addClass("number")
                 .attr({
                     "id": nowStatus[i]
-                        //"value": nowStatus[i]
                 });
         } else if (nowStatus[i] === 8) {
             $("[value=" + setStatus[i] + "]").text("9")
@@ -39,7 +38,6 @@ function set(nowStatus, setStatus) {
                 .addClass("special")
                 .attr({
                     "id": 8
-                        //"value": 8
                 });
         }
     }
@@ -77,7 +75,6 @@ $(document).ready(function() {
             if (nowStatus[p] != 8) {
                 var row = Math.floor(p / n);
                 var col = p % n;
-
                 //向上的情况
                 if (((p - n) >= 0) && (nowStatus[p - n] == 8)) {
                     nowStatus[p - n] = nowStatus[p];
@@ -91,7 +88,6 @@ $(document).ready(function() {
                     }, 100, function() {
                         successful();
                     });
-
                 }
                 //向下的情况
                 else if (((p + n) < (n * n)) && (nowStatus[p + n] == 8)) {
@@ -105,7 +101,6 @@ $(document).ready(function() {
                     }, 100, function() {
                         successful();
                     });
-
                 }
                 //向左的情况
                 else if (((p - 1) >= 0) && ((p - 1) < (n * n - 1)) && (nowStatus[p - 1] == 8)) {
@@ -119,7 +114,6 @@ $(document).ready(function() {
                     }, 100, function() {
                         successful()
                     });
-
                 }
                 //向右的情况
                 else if (((p + 1) > 0) && ((p + 1) < n * n) && (nowStatus[p + 1] == 8)) {
@@ -133,11 +127,9 @@ $(document).ready(function() {
                     }, 100, function() {
                         successful();
                     });
-
                 }
             }
         });
-
     })
     //一个document.ready下面只能有一个函数，不能同时有多个
     //用jQuery获取输入的表单值
@@ -147,11 +139,13 @@ $(document).ready(function() {
         nTemp = $("#box").val();
         var inputStatus = new Array();
         //先删除多余的元素
-        $("ul li:eq(8)").nextAll().remove();
-        //然后增加需要的元素
-        for (var i = 9; i < (nTemp * nTemp); i++) {
+        //$("ul li:eq(8)").nextAll().remove();
+        //先删除所有元素
+        $("ul").empty();
+        //然后增加需要数目的元素
+        for (var i = 0; i < (nTemp * nTemp); i++) {
             //注意在jQuery中所有变量的实现方法：除变量外全部加双引号，并且用加号连接
-            $("ul li:eq(" + (i - 1) + ")").after("<li>" + (i + 1) + "</li>");
+            $("ul").append("<li>" + (i + 1) + "</li>");
         }
         //最后给所有元素赋属性
         for (var j = 0; j < (nTemp * nTemp); j++) {
@@ -163,10 +157,13 @@ $(document).ready(function() {
                 "line-height": (600 / nTemp) + "px",
                 "border": "1px solid #696969",
                 "color": "#FFFFFF",
-                "positon":"relative",
-                "margin-right":"0px",
-                "display":"inline-block"
+                "positon": "relative",
+                "margin-right": "0px"
             })
+            .attr({
+                "id": j,
+                "value": j
+            });
         }
     });
 })
