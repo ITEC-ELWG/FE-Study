a830e91daa742afd1b4fe05483fb600c6dcc9f30
liugui <1169430612@qq.com>
2014-11-04 19:48:13 +0800

    实现了输入n时生成n*n个方格

--- liugui/Puzzle8/js/js.js
+++ liugui/Puzzle8/js/js.js
@@ -71,68 +71,98 @@ $(document).ready(function() {
 });
 
 $(document).ready(function() {
-    $("ul li").click(function() {
-        //valueGet()函数放在这里调用
-        var p = valueGet(this);
-        if (nowStatus[p] != 8) {
-            var row = Math.floor(p / n);
-            var col = p % n;
+        $("ul li").click(function() {
+            //valueGet()函数放在这里调用
+            var p = valueGet(this);
+            if (nowStatus[p] != 8) {
+                var row = Math.floor(p / n);
+                var col = p % n;
 
-            //向上的情况
-            if (((p - n) >= 0) && (nowStatus[p - n] == 8)) {
-                nowStatus[p - n] = nowStatus[p];
-                nowStatus[p] = 8;
-                $("#" + nowStatus[p - n]).animate({
-                    top: "-=200px"
-                }, 100);
-                $("#" + nowStatus[p]).animate({
-                    top: "+=200px"
-                }, 100,function(){
-                    successful();
-                });
-                
+                //向上的情况
+                if (((p - n) >= 0) && (nowStatus[p - n] == 8)) {
+                    nowStatus[p - n] = nowStatus[p];
+                    nowStatus[p] = 8;
+                    $("#" + nowStatus[p - n]).animate({
+                        top: "-=200px"
+                    }, 100);
+                    //callback函数不能直接调用，要用function代入
+                    $("#" + nowStatus[p]).animate({
+                        top: "+=200px"
+                    }, 100, function() {
+                        successful();
+                    });
+
+                }
+                //向下的情况
+                else if (((p + n) < (n * n)) && (nowStatus[p + n] == 8)) {
+                    nowStatus[p + n] = nowStatus[p];
+                    nowStatus[p] = 8;
+                    $("#" + nowStatus[p + n]).animate({
+                        top: "+=200px"
+                    }, 100);
+                    $("#" + nowStatus[p]).animate({
+                        top: "-=200px"
+                    }, 100, function() {
+                        successful();
+                    });
+
+                }
+                //向左的情况
+                else if (((p - 1) >= 0) && ((p - 1) < (n * n - 1)) && (nowStatus[p - 1] == 8)) {
+                    nowStatus[p - 1] = nowStatus[p];
+                    nowStatus[p] = 8;
+                    $("#" + nowStatus[p - 1]).animate({
+                        left: "-=200px"
+                    }, 100);
+                    $("#" + nowStatus[p]).animate({
+                        left: "+=200px"
+                    }, 100, function() {
+                        successful()
+                    });
+
+                }
+                //向右的情况
+                else if (((p + 1) > 0) && ((p + 1) < n * n) && (nowStatus[p + 1] == 8)) {
+                    nowStatus[p + 1] = nowStatus[p];
+                    nowStatus[p] = 8;
+                    $("#" + nowStatus[p + 1]).animate({
+                        left: "+=200px"
+                    }, 100);
+                    $("#" + nowStatus[p]).animate({
+                        left: "-=200px"
+                    }, 100, function() {
+                        successful();
+                    });
+
+                }
             }
-            //向下的情况
-            else if (((p + n) < (n * n)) && (nowStatus[p + n] == 8)) {
-                nowStatus[p + n] = nowStatus[p];
-                nowStatus[p] = 8;
-                $("#" + nowStatus[p + n]).animate({
-                    top: "+=200px"
-                }, 100);
-                $("#" + nowStatus[p]).animate({
-                    top: "-=200px"
-                }, 100,function(){
-                    successful();
-                });
-                
-            }
-            //向左的情况
-            else if (((p - 1) >= 0) && ((p - 1) < (n * n - 1)) && (nowStatus[p - 1] == 8)) {
-                nowStatus[p - 1] = nowStatus[p];
-                nowStatus[p] = 8;
-                $("#" + nowStatus[p - 1]).animate({
-                    left: "-=200px"
-                }, 100);
-                $("#" + nowStatus[p]).animate({
-                    left: "+=200px"
-                }, 100,function(){ successful()
-                });
-                
-            }
-            //向右的情况
-            else if (((p + 1) > 0) && ((p + 1) < n * n) && (nowStatus[p + 1] == 8)) {
-                nowStatus[p + 1] = nowStatus[p];
-                nowStatus[p] = 8;
-                $("#" + nowStatus[p + 1]).animate({
-                    left: "+=200px"
-                }, 100);
-                $("#" + nowStatus[p]).animate({
-                    left: "-=200px"
-                }, 100,function(){
-                    successful();
-                });
-                
-            }
+        });
+
+    })
+    //一个document.ready下面只能有一个函数，不能同时有多个
+    //用jQuery获取输入的表单值
+$(document).ready(function() {
+    $(".submit").bind("click", function() {
+        var nTemp = 0;
+        nTemp = $("#box").val();
+        var inputStatus = new Array();
+        //先删除多余的元素
+        $("ul li:eq(8)").nextAll().remove();
+        //然后增加需要的元素
+        for (var i = 9; i < (nTemp * nTemp); i++) {
+            $("ul li:eq(" + (i - 1) + ")").after("<li>" + i + "</li>");
+        }
+        //最后给所有元素赋属性
+        for (var j = 0; j < (nTemp * nTemp); j++) {
+            inputStatus[j] = j;
+            $("ul li:eq(" + j + ")").css({
+                "width": (600 / nTemp - 2) + "px",
+                "height": (600 / nTemp - 2) + "px",
+                "font-size": (24 / nTemp) + "em",
+                "line-height": (600 / nTemp) + "px",
+                "border": "1px solid",
+                "color": "#FFFFFF"
+            });
         }
     });
 })
