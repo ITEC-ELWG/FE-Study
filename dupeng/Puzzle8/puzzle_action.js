f9cb2961ab10c354243de6f2b499e9af7339dce6
arlendp <dupeng1160058696@qq.com>
2015-10-13 16:23:12 +0800

    完善移动部分的错误

--- dupeng/Puzzle8/puzzle_action.js
+++ dupeng/Puzzle8/puzzle_action.js
@@ -7,7 +7,7 @@ $(document).ready(function(){
 		Move(this,-3);
 		Win();
 	});
-	$(document).keyup(function(e){
+	$(document).keyup(function(e){//设置键盘的点击事件
 		if(e.keyCode==37){
 			for(var i=1;i<10;i++){
 				if(Move($(".grid"+i).get(0),-1)==true){
@@ -33,10 +33,11 @@ $(document).ready(function(){
 				}
 			}
 		}
+		Win();
 	});
-	function Move(node,step){
+	function Move(node,step){//根据当前被操作模块进行移动 step的值分别表示:上移(-3),左移(-1),右移(1),下移(3)。
 		var tag=parseInt(node.className.split("grid")[1]);
-		if($("p."+"grid"+(tag+step)).attr("class")!=undefined&&$("p."+"grid"+(tag+step)).attr("class").indexOf("blank")!=-1){
+		if(canMove(tag,step)&&$("p."+"grid"+(tag+step)).attr("class").indexOf("blank")!=-1){
 			var data;
 			var name;
 			name="p."+"grid"+(tag+step);
@@ -58,5 +59,14 @@ $(document).ready(function(){
 		}
 		alert("You win the game!");
 		window.location.reload();
+	}
+	function canMove(location,step){//判断特定位置能移动的方向,如位置1处只能向右、下两个方向移动。
+		if((location==1&&step==-1)||(location==1&&step==-3)||(location==2&&step==-3)||(location==3&&step==-3)||(location==3&&step==1)
+			||(location==4&&step==-1)||(location==6&&step==1)||(location==7&&step==-1)||(location==7&&step==3)
+			||(location==8&&step==3)||(location==9&&step==1)||(location==9&&step==3)){
+			return false;
+		}else{
+			return true;
+		}
 	}
 });
