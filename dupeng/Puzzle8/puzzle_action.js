8fd133246e56521d77a264fdb9da86dd518efbe6
arlendp <dupeng1160058696@qq.com>
2015-10-13 15:48:00 +0800

    增加键盘上下左右的移动控制

--- dupeng/Puzzle8/puzzle_action.js
+++ dupeng/Puzzle8/puzzle_action.js
@@ -1,9 +1,38 @@
 $(document).ready(function(){
+	Win();
 	$("p").click(function(){
 		Move(this,1);
 		Move(this,-1);
 		Move(this,3);
 		Move(this,-3);
+		Win();
+	});
+	$(document).keyup(function(e){
+		if(e.keyCode==37){
+			for(var i=1;i<10;i++){
+				if(Move($(".grid"+i).get(0),-1)==true){
+					break;
+				}
+			}
+		}else if(e.keyCode==38){
+			for(var i=1;i<10;i++){
+				if(Move($(".grid"+i).get(0),-3)==true){
+					break;
+				}
+			}
+		}else if(e.keyCode==39){
+			for(var i=1;i<10;i++){
+				if(Move($(".grid"+i).get(0),1)==true){
+					break;
+				}
+			}
+		}else if(e.keyCode==40){
+			for(var i=1;i<10;i++){
+				if(Move($(".grid"+i).get(0),3)==true){
+					break;
+				}
+			}
+		}
 	});
 	function Move(node,step){
 		var tag=parseInt(node.className.split("grid")[1]);
@@ -16,7 +45,18 @@ $(document).ready(function(){
 			$(name).text(data);
 			$(node).addClass("blank");
 			$(name).removeClass("blank");
+			return true;
+		}else{
+			return false;
 		}
 	}
-	
+	function Win(){
+		for(var i=1;i<9;i++){
+			if($("p.grid"+i).text()!=i){
+				return;
+			}
+		}
+		alert("You win the game!");
+		window.location.reload();
+	}
 });
