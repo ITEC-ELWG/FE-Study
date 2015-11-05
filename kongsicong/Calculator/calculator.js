fa0fe0856a280ec18e370d75d39b1753e36daea7
kongsicong <1390159970@qq.com>
2015-11-05 15:22:25 +0800

    modify the css style and the javascript function

--- kongsicong/Calculator/calculator.js
+++ kongsicong/Calculator/calculator.js
@@ -1,58 +1,53 @@
 
-window.onload=function(){
-	var show=document.getElementById("show");
-	var result=document.getElementById("result");
-	var AC=document.getElementById("AC");
-	var del=document.getElementById("delete");
-	var buttons=document.getElementsByTagName("button");
-	var toggle=document.getElementById("switch");
-	var str="";
-	for(var i=0;i<buttons.length;i++){
-		buttons[i].onclick=function(){
-			str=str+this.innerHTML;
-			show.value=str;
+window.onload = function(){
+	var show = document.getElementById("show");
+	var result = document.getElementById("result");
+	var AC = document.getElementById("AC");
+	var del = document.getElementById("delete");
+	var buttons = document.getElementsByTagName("button");
+	var toggle = document.getElementById("switch");
+	var str = "";
+	for(var i = 0;i < buttons.length;i++){
+		buttons[i].onclick = function(){
+			str = str + this.innerHTML;
+			show.value = str;
 		}
 	}
-	var count=1;
-	toggle.onclick=function(){
+	var count = 1;
+	toggle.onclick = function(){
 		
-		if(count%2==1){
-			for(var i=0;i<buttons.length;i++){
-				if(i!=3)buttons[i].disabled="true";
+		if(count%2 == 1){
+			for(var i = 0;i < buttons.length;i++){
+				if(i != 3)buttons[i].disabled = "true";
 			}
 		}
 		else {
-			for(var i=0;i<buttons.length;i++){
-				if(i!=3)buttons[i].disabled="";
+			for(var i = 0;i < buttons.length;i++){
+				if(i != 3)buttons[i].disabled = "";
 			}
 		}
-		show.value="";
+		show.value = "";
 		count++;
 	}
 	del.onclick=function(){
-		str=str.substr(0,str.length-1);
-		show.value=str;
+		str = str.substr(0,str.length-1);
+		show.value = str;
 	}
-	result.onclick=function(){
+	result.onclick = function(){
+		var res = 0;
 		var reg = /^(\d+(\.\d+)?[\+\-\*\/]\d+(\.\d+)?)|(\d+(\.\d+)?\%)$/;
-		var r=str.match(reg);
-		var res=0;
-		var substr;
-		if(r!=null){
-			substr=str.split(/[\+\-\*\/]/);
-			if(str.indexOf('+')!=-1)res=parseFloat(substr[0])+parseFloat(substr[1]);
-			if(str.indexOf('-')!=-1)res=parseFloat(substr[0])-parseFloat(substr[1]);
-			if(str.indexOf('*')!=-1)res=parseFloat(substr[0])*parseFloat(substr[1]);
-			if(str.indexOf('/')!=-1)res=parseFloat(substr[0])/parseFloat(substr[1]);
-			if(str.indexOf('%')!=-1)res=parseFloat(substr[0])/100;
-			show.value=res.toString();
-		}else{
-			show.value="input wrong";
+		var r = str.match(reg);
+		if(r!=null) {
+			if(str.indexOf('%') != -1) res = parseFloat(str.split(/[%]/)[0]) / 100;
+			else res = eval(str);
+			show.value = res;
+		}else {
+			res = "input wrong"
 		}
-		str="";
+		str = "";
 	}
-	AC.onclick=function(){
-		show.value="0";
-		str="";
+	AC.onclick = function(){
+		show.value = "0";
+		str = "";
 	}
 }
