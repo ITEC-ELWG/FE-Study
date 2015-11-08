99d859e5897b5b99f39011383dc79550f3efc939
zhangyan123 <1450802680@qq.com>
2015-11-08 16:38:45 +0800

    更改时间设置
    使用setTimeout函数动态显示时间

--- zhangyan/Calculator/index.js
+++ zhangyan/Calculator/index.js
@@ -1,5 +1,23 @@
-﻿1 
-document.getElementById("time").innerHTML=document.getElementById("time").innerHTML+Date();
+﻿
+function startTime()
+{
+var today=new Date()
+var h=today.getHours()
+var m=today.getMinutes()
+var s=today.getSeconds()
+// add a zero in front of numbers<10
+m=checkTime(m)
+s=checkTime(s)
+document.getElementById('time').innerHTML=h+":"+m+":"+s
+t=setTimeout('startTime()',500)
+}
+
+function checkTime(i)
+{
+if (i<10) 
+  {i="0" + i}
+  return i
+}
 
 var num = 0,
     result = 0,
