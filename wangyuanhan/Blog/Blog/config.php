ab1c7e9ca8e9b8bc8378941ddbb0aed43037d5e4
YX <393585468@qq.com>
2015-11-24 20:19:19 +0800

    【Blog(PHP)】首次提交

---
+++ wangyuanhan/Blog/Blog/config.php
@@ -0,0 +1,5 @@
+<?php
+
+define('MYSQL_HOST', 'localhost');
+define('MYSQL_USER', 'root');
+define('MYSQL_PASSWORD', '');
---
+++ wangyuanhan/Blog/Blog/test.php
@@ -0,0 +1,9 @@
+<?php
+if(!filter_var("£¬", FILTER_SANITIZE_EMAIL))
+{
+	echo "E-mail is not valid";
+}
+else
+{
+	echo "E-mail is valid";
+}
