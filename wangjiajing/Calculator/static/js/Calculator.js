c9792cca510439634a1af9d51af40e9806ea8b48
missMHWGO <2486148831@qq.com>
2015-12-24 21:46:43 +0800

    只能输入一个小数点啦~可以选择第二个数的符号~第三个问题我再看看，感觉第三个问题会要修改很多地方~

--- wangjiajing/Calculator/static/js/Calculator.js
+++ wangjiajing/Calculator/static/js/Calculator.js
@@ -9,17 +9,32 @@ var inputArr = new Array;
 var sign = "";
 
 //显示
+//判断是否只有一个小数点
+var adot = function() {
+    for (var i = 0; i < input[0].value.length; i++) {
+        if (input[0].value[i] == ".") {
+            return true;
+        }
+    }
+    return false;
+}
+
 //实现不在数字前面显示 0，如不会显示 0023这样的数字，即使输入 0023，也只显示 23
 var link = function() {
     var strLen = str.length + 1;
     str += this.innerHTML.toString();
-    for (var i = 0; i < strLen; i++) {
-        if (str[i] == 0 && str[i + 1] >= 0 && str[i + 1] <= 9) {
-            continue;
-        } else {
-            str = str.substring(i, strLen);
-            input[0].value = str;
-            return;
+    if (this.innerHTML == "." && adot()) {
+        str = str.substring(0, strLen - 1);
+        input[0].value = str;
+    } else {
+        for (var i = 0; i < strLen; i++) {
+            if (str[i] == 0 && str[i + 1] >= 0 && str[i + 1] <= 9) {
+                continue;
+            } else {
+                str = str.substring(i, strLen);
+                input[0].value = str;
+                return;
+            }
         }
     }
 }
@@ -28,8 +43,6 @@ var show = function() {
     for (var i = 0; i < btnLen; i++) {
         if (buttons[i].innerHTML >= 0 && buttons[i].innerHTML <= 9 || buttons[i].innerHTML == ".") {
             buttons[i].onclick = link;
-        } else {
-            str = "";
         }
     }
 }
@@ -74,8 +87,6 @@ for (var i = 2; i < 6; i++) {
         } else if (inputArr[0] == "" && this.innerHTML == symbol[5].innerHTML) {
             input[0].value = "+";
             str = "+";
-        } else if (sign != "") {
-            alert("这是一个计算两个数的计算器！");
         } else if ((this.innerHTML == symbol[2].innerHTML || this.innerHTML == symbol[3].innerHTML) && inputArr[0] == "") {
             alert("请输入正确的表达式！");
         } else {
