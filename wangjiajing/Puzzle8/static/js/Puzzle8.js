7b958de2e99d9767a9305d2052fdd92f0cf297ee
missMHWGO <2486148831@qq.com>
2015-12-28 20:54:51 +0800

    输入n可以生成n*n的棋盘啦~在写js的过程中根据需要调整了一点点html和css~~~

--- wangjiajing/Puzzle8/static/js/Puzzle8.js
+++ wangjiajing/Puzzle8/static/js/Puzzle8.js
@@ -1,22 +1,61 @@
-// var blockArr = $(".block");
-
 $(document).ready(function() {
+    var n;
+
+    //重置
+    $(".input")[1].onclick = function() {
+        //先清除再生成
+        $(".matrix").empty();
+        n = parseInt($(".input")[0].value);
+        if (isNaN(n)) {
+            alert("请输入n!");
+        } else if (n <= 2 || n >= 10) {
+            alert("n的取值应为（2<n<10）!");
+        } else {
+            generate();
+            var arr = getrandom();
+            getrandomshow(arr);
+            for (var i = 0; i < n * n; i++) {
+                $(".block")[i].onclick = move;
+            }
+        }
+    }
+
+    //生成n*n的棋盘
+    var generate = function() {
+        for (var i = 0; i < n; i++) {
+            var rowStr = "";
+
+            rowStr += '<div class="row' + i + '">' + '</div>';
+            $(".matrix").append(rowStr);
+            for (var j = 0; j < n; j++) {
+                var blockStr = "";
+
+                blockStr += '<button class="block"></button>';
+                $(".row" + i).append(blockStr);
+            };
+        };
+        $($(".block")[n * n - 1]).addClass("move");
+    }
+
     //生成随机数组
     var getrandom = function() {
-        var array = new Array(4, 5, 3, 1, 9, 8, 7, 6, 2);
+        var array = new Array;
+        for (var i = 0; i < n * n; i++) {
+            array[i] = i;
+        };
         var randomsort = function() {
             return Math.random() > 0.5 ? -1 : 1;
         }
         return array.sort(randomsort);
     }
-    var arr = getrandom();
 
-    var getrandomshow = function() {
+    //将随机数组值赋给棋盘
+    var getrandomshow = function(arr) {
         var move0 = $($(".move")[0]);
-        for (var i = 0; i < 9; i++) {
+        for (var i = 0; i < n * n; i++) {
             var blocki = $($(".block")[i]);
-            blocki.html(arr[i]);
-            if (blocki.html() == 9) {
+            blocki.html(arr[i] + 1);
+            if (blocki.html() == n * n) {
                 blocki.html("");
                 blocki.addClass("move");
             }
@@ -26,13 +65,12 @@ $(document).ready(function() {
         }
 
     }
-    getrandomshow();
 
     //判断点击的块能不能移动
     var canmove = function(cur) {
-        for (var i = 0; i < 9; i++) {
+        for (var i = 0; i < n * n; i++) {
             if ($(".block")[i] == cur) {
-                if ($(".block")[i - 3] == $(".move")[0] || $(".block")[i - 1] == $(".move")[0] || $(".block")[i + 1] == $(".move")[0] || $(".block")[i + 3] == $(".move")[0]) {
+                if ($(".block")[i - n] == $(".move")[0] || $(".block")[i - 1] == $(".move")[0] || $(".block")[i + 1] == $(".move")[0] || $(".block")[i + n] == $(".move")[0]) {
                     return true;
                 } else {
                     return false;
@@ -53,37 +91,31 @@ $(document).ready(function() {
         }
     }
 
-    for (var i = 0; i < 9; i++) {
-        $(".block")[i].onclick = move;
-    }
-
     //提示赢了
     var win = function() {
-        for (var i = 0; i < 8; i++) {
+        for (var i = 0; i < n * n - 1; i++) {
             var blocki = $($(".block")[i]);
             if (blocki.html() != i + 1) {
                 return;
             }
         }
-        alert("You win!");
+        if (!isNaN(n)) {
+            alert("You win!");
+        }
+
     }
     win();
-
-    //重置
-    $(".input")[1].onclick = function() {
-        window.location = "./Puzzle8.html";
-    }
 
     //键盘移动
     $(document).keyup(function(e) {
         var move0 = $($(".move")[0]);
 
-        for (var i = 0; i < 9; i++) {
+        for (var i = 0; i < n * n; i++) {
             if ($(".move")[0] == $(".block")[i]) {
                 switch (e.keyCode) {
                     case 37:
                         var left = $($(".block")[i - 1]);
-                        if (i != 0 && i != 3 && i != 6) {
+                        if (i != 0) {
                             move0.html(left.html());
                             left.html("");
                             left.addClass("move");
@@ -92,8 +124,8 @@ $(document).ready(function() {
                         };
                         break;
                     case 38:
-                        var top = $($(".block")[i - 3]);
-                        if (i != 0 && i != 1 && i != 2) {
+                        var top = $($(".block")[i - n]);
+                        if (i >= n) {
                             move0.html(top.html());
                             top.html("");
                             top.addClass("move");
@@ -103,7 +135,7 @@ $(document).ready(function() {
                         break;
                     case 39:
                         var right = $($(".block")[i + 1]);
-                        if (i != 2 && i != 5 && i != 8) {
+                        if (i != n * n - 1) {
                             move0.html(right.html());
                             right.html("");
                             right.addClass("move");
@@ -112,8 +144,8 @@ $(document).ready(function() {
                         };
                         break;
                     case 40:
-                        var bottom = $($(".block")[i + 3]);
-                        if (i != 6 && i != 7 && i != 8) {
+                        var bottom = $($(".block")[i + n]);
+                        if (i < n * n - n) {
                             move0.html(bottom.html());
                             bottom.html("");
                             bottom.addClass("move");
@@ -125,5 +157,4 @@ $(document).ready(function() {
             }
         }
     });
-
 });
