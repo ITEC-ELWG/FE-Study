823bbf74e7f9c8ba2b333d10244cb1f7c468cd3b
joyyuanxm <JOYyuan@JOYyuan.local>
2015-12-28 13:58:07 +0800

    login页面的修改

--- yuanxiaomei/blog/register.php
+++ yuanxiaomei/blog/register.php
@@ -1,47 +1,23 @@
 <!DOCTYPE html>
-
+<html>
 <head>
     <meta charset="utf-8">
-    <title>register</title>
-    <meta name="description" content="register">
-    <meta name="joyyuanxm" content="register">
+    <title>Login</title>
+    <meta name="description" content="Login">
+    <meta name="joyyuanxm" content="login for my index">
     <link rel="stylesheet" type="text/css" href="css/register.css" />
     <link rel="shortcut icon" href="images/title.png">
 </head>
 
 <body>
-<div id="register" class="reg">
-    <div class="reg2">
-        <div class="reg3">
-            <div class="reg_form">
-                <h1 class="h_title">注册</h1>
-                <p class="form_p">
-                    请填写个人信息
-                </p>
-                <form class="register-form">
-                    <fieldset>
-                        <legend class="sr">完善个人信息完成注册</legend>
-                        <div class="field-group">
-                            <label class="sr" for="name">姓名</label>
-                            <input name="name" class="field" id="name" type="text" placeholder="Name">
-                        </div>
-                        <div class="field-group">
-                            <label class="ps" for="password">密码</label>
-                            <input type="password" class="field" id="password" placeholder="Password">
-                        </div>
-                        <div class="field-group">
-                            <label class="sr" for="subject">账号</label>
-                            <input name="account" class="field" id="account" type="text" placeholder="Account">
-                        </div>
-                        <div class="text_right">
-                            <input type="submit" class="reg_btn" value="register">
-                        </div>
-                    </fieldset>
-                </form>
-            </div>
-        </div>
-    </div>
-</div>
-</body>
+<h1 class="h_title">register</h1>
+<form class="form" action="login.html">
+    <input type="text" class="field" placeholder="Username">
+    <input type="text" class="field" placeholder="Account">
+    <input type="password" class="field" placeholder="Password">
+    <input type="submit" class="field" id="login-button1" value="提交信息">
+</form>
 
+
+</body>
 </html>
