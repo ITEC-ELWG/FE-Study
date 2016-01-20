3029061c7120626b0b820afdded92f52f00ab165
joyyuanxm <JOYyuan@JOYyuan.local>
2016-01-20 20:40:20 +0800

    计算器多步计算

--- yuanxiaomei/caculator/js/caculator.js
+++ yuanxiaomei/caculator/js/caculator.js
@@ -1,162 +1,162 @@
-
-var num_in = "";
-var cal_in = "";
-var res_in = "";
-var num_out = "";
-var cal_out = "";
-//数组对象
-var cal_end = new Array();
-var num_end = new Array();
-var result;
 window.onload = function () {
-    function init() {
-        var inputs;
-        result = document.getElementById('display');
-        inputs = document.getElementsByTagName('input');
-        result.value = "0";
-        num_in = "0";
-        for (var i = 1; i < inputs.length; i++) {
-            inputs[i].onclick = function () {
-                selector(this);
-            }
-        }
+    var displayIn = document.getElementById('displayIn');
+    var displayOut = document.getElementById('displayOut');//显示结果
+    var equal = document.getElementById('equal');//等号按键
+    var AC = document.getElementById('AC');//清零按键
+    var del = document.getElementById('del');//删除输入按键
+    var plus = document.getElementById('plus');//+按键
+    var minus = document.getElementById('minus');//-按键
+    var multiply = document.getElementById('multiply');//*按键
+    var divide = document.getElementById('divide');//除号按键
+    var percent = document.getElementById('percent');
+    var point = document.getElementById('point');
+    var turnOn = document.getElementById('on');
+    var num = document.getElementsByClassName('btn_in num');//数字按键
+    var buttons = document.getElementsByTagName('button');
+    var cal = document.getElementsByClassName('btn_in cal');
+    var str = "";//控制displayout的显示，输入的数字和最终结果
+    var numA = null;//保存输入的操作数1
+    var calMAKE = null;//保存输入的操作数2
+    var numB = null;//保存输入的运算符
+    var equation = "";//控制displayin的显示，控制算式的输出
+    displayIn.value = str;
+    displayOut.value = equation;
+    turnOn.onclick = function () {
+        displayOut.value = '0';
+        displayIn.value = '';
+        str = '';
+        equation = '0';
+        numA = null;
+        calMAKE = null;
+        numB = null;
     }
-    init();
-}
-function selector(whichpre) {
-    switch (whichpre.className) {
-        case "btn_in num":
-            res_in = "";
-            if (cal_in) {
-                result.value = "";
-                cal_in= "";
-                num_in = "";
-            }
-            if ((whichpre.value == ".") && (num_in[1] == ".")) {
-                return;
-            }
-            if ((!num_in) && ((whichpre.value) == ".")) {
-                return;
-            }
+    del.onclick = function () {
+        equation = equation.substr(0, equation.length - 1);
+        displayOut.value = equation;
+    }
+    AC.onclick = function () {
+        displayOut.value = '0';
+        displayIn.value = '';
+        str = '';
+        equation = '0';
+        numA = null;
+        calMAKE = null;
+        numB = null;
+    }
+    percent.onclick = function () {
+        if (!isNaN(parseFloat(equation))) {
+            equation = parseFloat(equation) / 100;
+            equation = equation.toString();
+            displayOut.value = equation;
 
-            else if ((whichpre.value != ".") && (num_in == "0")) {
-                num_in = whichpre.value;
-                ifNumPush();
-                result.value = num_in;
+        }
+    }
+    for (var i = num.length - 1; i >= 0; i--) {
+        num[i].onclick = function () {
+            if (equation.indexOf('.') != -1 && this.innerHTML == '.')return;
+            if (equation == '' && this.innerHTML == '.') {
+                equation = '0.';
+                displayOut.value = equation;
                 return;
             }
-            else {
-                num_in += whichpre.value;
-                result.value = num_in;
-                ifNumPush();
+            if (numA != null && calMAKE == null) numA = null;
+            equation = equation + this.innerHTML;
+            displayOut.value = equation;
+        }
+    }
+    ;
+    //绑定运算符的点击事件
+    for (var i = cal.length - 1; i >= 0; i--) {
+        cal[i].onclick = function () {
+            //事件一：numA,calMAKE,numB为空
+            if (numA == null && calMAKE == null && numB == null) {
+                numA = parseFloat(equation);
+                calMAKE = this.innerHTML;
+                str = equation + calMAKE;
+                displayIn.value = str;
+                equation = '';
             }
-            break;
-        case "btn_in cal":
-            if (res_in) {
-                num_end.push(res_in);
-                num_in = res_in;
+            //事件二：输入了numA,calMAKE,等待numB的输入，在displayIn中显示已输入的算式
+            else if (numA != null && calMAKE != null && numB == null && isNaN(parseFloat(equation))) {
+                equation = this.innerHTML;
+                str = str.substr(0, str.length - 1) + calMAKE;
+                displayIn.value = str;
             }
-            if (num_in) {
-                cal_in = whichpre.value;
-                result.value = cal_in;
-                ifCalPush();
-            }
-            break;
-        case "btn_in equal":
-            calculate();
-            break;
-        case "btn_in del":
-            if ((!cal_in) && (!res_in)) {
-                num_in = num_in.substring(0, num_in.length - 1);
-                num_end[num_end.length - 1] = num_in;
-                if (num_in == "") {
-                    num_in = 0;
+            else if (numA !=null && calMAKE!=null && numB == null){
+                numB = parseFloat(equation);
+                str = str + equation;
+                switch (calMAKE){
+                    case "+" : equation = numA + numB; break;
+                    case "-" : equation = numA - numB; break;
+                    case "*" : equation = numA * numB; break;
+                    case "/" : equation = numA / numB; break;
                 }
-                ifNumPush();
-                result.value = num_in;
+               displayOut.value = equation;
+                calMAKE = this.innerHTML;
+                str = str + calMAKE;
+                displayIn.value = str;
+                numA = equation;
+                equation = "";
+                numB = null;}
+            else if (numA != null && calMAKE == null) {
+                calMAKE = this.innerHTML;
+                str = numA + calMAKE;
+                displayIn.value = str;
+                equation = '';
+                numB = null;
             }
-            break;
-        case "btn_in clear":
-           cal_in = "";
-            num_in = "";
-            res_in = "";
-            lastCal = "";
-            num_end.splice(0, num_end.length);
-            cal_end.splice(0, cal_end.length);
-            result.value = "0";
-            break;
-    }
-}
-function calculate() {
-    if ((!cal_end.length) && (!lastCal)) {
-        if (!num_in) {
-            num_in = "0";
-        }
-        result.value = num_in;
-        return;
-    }
-    else if (cal_end.length) {
-        res_in = num_end[0];
-        if (cal_end.length == num_end.length) {
-            num_end.push(num_end[num_end.length - 1]);
-        }
-        for (var i = 0; i < cal_end.length; i++) {
-           res_in = calselector(res_in, cal_end[i], num_end[i + 1]);
+            isFirst = true;
         }
     }
-    else {
-        cal_end.push(lastCal);
-        num_end.push(res_in);
-        num_end.push(lastNum);
-        calculate();
-        return;
+    ;
+    // 处理等号事件
+    var isFirst = true;
+    equal.onclick = function () {
+        if (numA != null && calMAKE != null && numB == null && isNaN(parseFloat(equation))) {
+            if (isFirst == true) var numMid = numA;
+            switch (calMAKE) {
+                case '+' :
+                    equation = numA + numMid;
+                    break;
+                case '-' :
+                    equation = numA - numMid;
+                    break;
+                case '*' :
+                    equation = numA * numMid;
+                    break;
+                case '÷' :
+                    equation = numA / numMid;
+                    break;
+            }
+            displayOut.value = equation;
+            numA = parseFloat(equation);
+            numB = null;
+            equation = '';
+            isFirst = false;
+        }
+        else if (numA != null && calMAKE != null && numB == null) {
+            numB = parseFloat(equation);
+            switch (calMAKE) {
+                case '+' :
+                    equation = numA + numB;
+                    break;
+                case '-' :
+                    equation = numA - numB;
+                    break;
+                case '*' :
+                    equation = numA * numB;
+                    break;
+                case '÷' :
+                    equation = numA / numB;
+                    break;
+            }
+            displayOut.value = equation;
+            numA = parseFloat(equation);
+            equation = '';
+            numB = null;
+            calMAKE = null;
+        }
+        str = '';
+        displayIn.value = '';
     }
-    if (res_in == "Infinity") {
-       res_in = "illegal";
-    }
-    result.value = res_in;
-    num_in = "";
-    num_end.splice(0, num_end.length);
-    cal_end.splice(0, cal_end.length);
-}
-function ifNumPush() {
-    if (num_end.length > cal_end.length) {
-        num_end[num_end.length - 1] = num_in;
-    }
-    else {
-        num_end.push(num_in);
-    }
-    num_out= num_end[num_end.length - 1];
-}
-function ifCalPush() {
-    if (num_end.length > cal_end.length) {
-        cal_end.push(cal_in);
-    }
-    else {
-        cal_end[cal_end.length - 1] = cal_in;
-    }
-   cal_out = cal_end[cal_end.length - 1];
-}
-function calselector(numA, cal, numB) {
-    var res_in = numA + cal + numB;
-    var anum = parseFloat(numA);
-    var bnum = parseFloat(numB);
-    switch (cal) {
-        case "+":
-            res_in = anum + bnum;
-            break;
-        case "-":
-            res_in = anum - bnum;
-            break;
-        case "*":
-            res_in = anum * bnum;
-            break;
-        case "÷":
-            res_in= anum / bnum;
-            break;
-        case "%":
-            res_in = anum / 100;
-            break;
-    }
-    return res_in;
 }
