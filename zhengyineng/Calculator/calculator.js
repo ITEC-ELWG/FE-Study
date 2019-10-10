527a9b64bdee8af558ebafdd95416a3f638a3c6b
zyn <rachel@ZYN-m.local>
2019-10-10 15:31:18 +0800

    连续计算 && 小数点问题

--- zhengyineng/Calculator/calculator.js
+++ zhengyineng/Calculator/calculator.js
@@ -1,23 +1,30 @@
 window.onload = function(){
 
-    num1 = null;
-    var num2 = null;
+    var num1 = null;
     var operator = null;
-    var resvar = '';
-    var state = 0;
+    var operator_flag = false;
+    var equal = 0;
     var disp = document.getElementsByClassName("display-box")[0];
 
     // 加减乘除
     document.getElementsByClassName("add")[0].onclick = function(){
+        operator_flag = true;
+        calculate();
         operator = 1;
     }
     document.getElementsByClassName("sub")[0].onclick = function(){
+        operator_flag = true;
+        calculate();
         operator = 2;
     }
     document.getElementsByClassName("multi")[0].onclick = function(){
+        operator_flag = true;
+        calculate();
         operator = 3;
     }
     document.getElementsByClassName("divide")[0].onclick = function(){
+        operator_flag = true;
+        calculate();
         operator = 4;
     }
 
@@ -26,58 +33,84 @@ window.onload = function(){
     }
 
     // 数字按键
-    for(var i = 0; i < 11; i ++){
+    for(var i = 0; i < 10; i ++){
         document.getElementsByClassName("num")[i].onclick = function(){
-            if(operator == null){
+            if(operator_flag == true || equal == 1){
+                // num1 = disp.value;
+                disp.value = this.innerHTML;
+                operator_flag = false;
+                equal = 0;
+            } else { 
                 disp.value = disp.value + this.innerHTML;
-                num1 = disp.value;
-            } else{
-                if(state==0){
-                    disp.value = this.innerHTML;
-                    num2 = disp.value;
-                    state++;
-                } else if(state >= 1){
-                    disp.value = disp.value + this.innerHTML;
-                    num2 = disp.value;
-                }
             }
         }
+    }
+
+    //小数点
+    document.getElementsByClassName("dot")[0].onclick = function () {
+        if (checkdot()) {
+            if (disp.value == "") {
+                disp.value = "0" + ".";
+            }
+            else {
+                disp.value = disp.value + ".";
+            }
+        }
+        else {
+            return false;
+        }
+    }
+    function checkdot () {
+        for(var i=0; i <= disp.value.length; i++) {
+            if(disp.value.substr(i, 1) == ".") 
+                return false;  
+        }
+        return true;
     }
 
     // 清屏
     document.getElementsByClassName("clear")[0].onclick = function(){
         num1 = null;
-        num2 = null;
         operator = null;
+        operator_flag = false;
+        equal = 0;
         res = '';
-        state = 0;
         disp.value = null;
     }
 
     // 计算
-    document.getElementsByClassName("equal")[0].onclick = function(){
-        switch(operator){
-            case 1:
-                res = Number(num1) + Number(num2);
-                disp.value = res;
-                break;
-            case 2:
-                res = Number(num1) - Number(num2);
-                disp.value = res;
-                break;
-            case 3:
-                res = Number(num1) * Number(num2);
-                disp.value = res;
-                break;
-            case 4:
-                if(Number(num2) == 0){
-                    disp.value = "Error!";
-                    break;
-                }else{
-                    res = Number(num1) / Number(num2);
-                    disp.value = res;
-                    break;
-                }  
+    function calculate(){
+        if(operator == null){
+            num1 = disp.value;
         }
+        if(num1 != null){
+            switch(operator){
+                case 1:
+                    num1 = Number(num1) + Number(disp.value);
+                    break;
+                case 2:
+                    num1 = Number(num1) - Number(disp.value);
+                    break;
+                case 3:
+                    num1 = Number(num1) * Number(disp.value);
+                    break;
+                case 4:
+                    if(Number(disp.value) == 0){
+                        disp.value = "Error!";
+                        break;
+                    }else{
+                        num1 = Number(num1) / Number(disp.value);
+                        break;
+                    }  
+            }
+            disp.value = String(num1);
+            operator = null;
+        }
+        
+    }
+    document.getElementsByClassName("equal")[0].onclick = function(){
+        equal = 1;
+        calculate();
+        num1 = 0;
     }
 }
